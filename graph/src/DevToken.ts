d873e2ecb3b80439d57132671b0ced293bd0ab0f
marcialarturo <87619763+marcialarturo@users.noreply.github.com>
2023-09-24 09:20:37 -0400

    add The Graph

---
+++ graph/src/DevToken.ts
@@ -0,0 +1,121 @@
+import {
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  BatchMetadataUpdate as BatchMetadataUpdateEvent,
+  ContributionCreated as ContributionCreatedEvent,
+  MetadataUpdate as MetadataUpdateEvent,
+  Transfer as TransferEvent
+} from "../generated/DevToken"
+import {
+  Approval,
+  ApprovalForAll,
+  BatchMetadataUpdate,
+  ContributionCreated,
+  MetadataUpdate,
+  Transfer
+} from "../generated/schema"
+
+export function handleProfile(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.approved = event.params.approved
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  let entity = new ApprovalForAll(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.operator = event.params.operator
+  entity.approved = event.params.approved
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBatchMetadataUpdate(
+  event: BatchMetadataUpdateEvent
+): void {
+  let entity = new BatchMetadataUpdate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._fromTokenId = event.params._fromTokenId
+  entity._toTokenId = event.params._toTokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleContributionCreated(
+  event: ContributionCreatedEvent
+): void {
+  let entity = new ContributionCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.id = event.params.id
+  entity.cid = event.params.cid
+  entity.organizer = event.params.organizer
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMetadataUpdate(event: MetadataUpdateEvent): void {
+  let entity = new MetadataUpdate(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity._tokenId = event.params._tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.tokenId = event.params.tokenId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
